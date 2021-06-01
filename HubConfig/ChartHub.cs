a92fb3c3637fea37a693b817b8e72029001f15ee
Igor Korniyenko <igor.krnyk@gmail.com>
2021-06-01 18:10:23 +0200

    envia datos a clientes seleccionados

---
+++ HubConfig/ChartHub.cs
@@ -0,0 +1,17 @@
+﻿using DemoSignalR.Models;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoSignalR.HubConfig
+{
+    public class ChartHub : Hub
+    {
+        public async Task BroadcastChartData(List<ChartModel> data, string connectionId)
+        => await Clients.Client(connectionId).SendAsync("broadcastchartdata", data);
+        public string GetConnectionId() => Context.ConnectionId;
+
+    }
+}
